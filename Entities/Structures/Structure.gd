ba8bfe6d22d690298560cbe095d5ec6c26af0b51
squiffel <qytiswork@gmail.com>
2023-10-27 21:44:40 -0400

    Player can walk over structures and push enemies

--- Entities/Structures/Structure.gd
+++ Entities/Structures/Structure.gd
@@ -1,42 +1,36 @@
 extends Node2D
 
 @onready var area_2d : Area2D = $Area2D
-@onready var collision_shape_2d : CollisionShape2D = $Area2D/CollisionShape2D
+@onready var area_2d_collision_shape_2d : CollisionShape2D = $Area2D/CollisionShape2D
 
-# Building
-var construction_started : bool = false
+@onready var static_body_2d : StaticBody2D = $StaticBody2D
+@onready var static_body_2d_collision_shape_2d : CollisionShape2D = $StaticBody2D/CollisionShape2D
 
-# Padding
-var padding = 8
+# Building construction
+var is_building_constructed : bool = false
 
 # Objects in build area
 var nodes_in_build_area_list : Array = []
 
 func _ready():
-	collision_shape_2d.shape.size = Vector2(GameGrid.cell_size - padding, GameGrid.cell_size - padding)
-	# Show as semi-transparent when building
-	modulate.a = 0.4
+	# Create the collision mask and player detection
+	var padding = 8
+	var collision_shape_size : Vector2 = Vector2(GameGrid.cell_size - padding, GameGrid.cell_size - padding)
+	area_2d_collision_shape_2d.shape.size = collision_shape_size
+	static_body_2d_collision_shape_2d.shape.size = collision_shape_size
+	# If the area is clear create the building
+	_initialize_building_construction()
 
 func _on_area_2d_body_entered(body):
 	nodes_in_build_area_list.push_back(body)
 
 func _on_area_2d_body_exited(body):
 	nodes_in_build_area_list.erase(body)
-	if nodes_in_build_area_list.size() == 0:
-		# Convert to Static body
-		var static_body_2d : StaticBody2D = StaticBody2D.new()
-		add_child(static_body_2d)
-		# Static body 2d Collision Sape
-		call_deferred("_add_collision_shape", static_body_2d)
-		# Remove Area 2D
-		area_2d.queue_free()
-		# Construction has to started
-		modulate.a = 1.0 # Make fully visible
-		construction_started = true
+	_initialize_building_construction()
 
-func _add_collision_shape(static_body_2d):
-	# Static body 2d Collision Sape
-	var static_body_2d_collision_shape_2d : CollisionShape2D = CollisionShape2D.new()
-	static_body_2d_collision_shape_2d.shape = RectangleShape2D.new()
-	static_body_2d_collision_shape_2d.shape.size = Vector2(GameGrid.cell_size, GameGrid.cell_size)
-	static_body_2d.add_child(static_body_2d_collision_shape_2d)
+func _initialize_building_construction():
+	if not is_building_constructed:
+		# If there is no objects colliding. Turn collision rectangle on
+		static_body_2d_collision_shape_2d.disabled = nodes_in_build_area_list.size() > 0
+		modulate.a = 1.0 if nodes_in_build_area_list.size() == 0 else 0.4
+		is_building_constructed = true
