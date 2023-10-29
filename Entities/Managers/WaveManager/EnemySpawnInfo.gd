c3c985c25f6e1d41a8e113fb2d9663d198a559dc
Carlos Michael <carlosmichaeldev@gmail.com>
2023-10-29 14:51:30 -0400

    added wave manager pass 1

--- Entities/Managers/WaveManager/EnemySpawnInfo.gd
+++ Entities/Managers/WaveManager/EnemySpawnInfo.gd
@@ -4,6 +4,6 @@ extends Resource
 @export var type: String
 @export var time: float
 @export var spawn_point_name: String
-@export var position: Vector3
+@export var position: Vector2
 @export var repeat_time: float
 @export var number_to_spawn: int
--- Entities/Managers/WaveManager/WaveManager.gd
+++ Entities/Managers/WaveManager/WaveManager.gd
@@ -9,21 +9,71 @@ var timer = 0.0
 # each wave is an array of enemies (represented by EnemySpawnInfo)
 # each enemy has a type, position, and time to spawn
 @export var waves: Array[Wave]
+# current spawn index
+var spawn_index: int = 0
+
+# spawn waves algorithm
+# for each wave in waves
+# 	display wave.description and wave.name
+# 	for each enemy in wave.enemies
+# 		wait enemy.time
+# 		spawn enemy.type at enemy.position
+# 		for each repeat in enemy.number_to_spawn
+# 			wait enemy.repeat_time
+# 			spawn enemy.type at enemy.position
+
+# variable to store all alive enemyes in current wave
+var alive_enemies: Array[PackedScene] = []
+# function that checks alive enemies and returns true if all enemies are destroyed
+func is_wave_complete():
+	if (spawning):
+		return false
+	if alive_enemies.size() == 0:
+		return false
+	for enemy in alive_enemies:
+		if enemy != null || !enemy.is_queued_for_deletion():
+			return false
+
+	print("wave complete no more enemies")
+	return true
+
+var spawning = false
+
+func _spawn_wave(wave: Wave):
+	spawning = true;
+	print("spawning" + str(wave.name))
+	print(wave.name)
+	print(wave.description)
+	for enemy_info in wave.enemies:
+		await get_tree().create_timer(enemy_info.time).timeout
+		for i in range(enemy_info.number_to_spawn):
+			var enemy_instance = enemy_scene.instantiate()
+			enemy_instance.position = enemy_info.position
+			print("spawning "+ enemy_info.type + "at " + str(enemy_info.position))
+			add_child(enemy_instance)
+			alive_enemies.append(enemy_instance)
+			await get_tree().create_timer(enemy_info.repeat_time).timeout
+	spawning = false			
+	print("all enemies in this wave have been spawned")
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# every 5 seconds, spawn an enemy_scene
-	spawn_enemy()
+	_spawn_wave(waves[0])
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	timer += delta
-	if (timer >= spawn_timer):
-		spawn_enemy()
-		timer = 0.0
+	if (is_wave_complete()):
+		spawn_index += 1
+		if (spawn_index >= waves.size()):
+			spawn_index = 0
+			print("All waves complete!")
+		else:
+			_spawn_wave(waves[spawn_index])
 	pass
 
 func spawn_enemy():
