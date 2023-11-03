116b0791c4b3f5f98582b651395bd51d9daa732d
squiffel <qytiswork@gmail.com>
2023-11-02 21:33:39 -0400

    Sound fix

--- Entities/Weapons/WeaponBase.gd
+++ Entities/Weapons/WeaponBase.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var sound_player : AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var shot_delay_timer : Timer = $ShotDelayTimer
 
+var sound_shoot = load("res://Entities/Weapons/Sounds/Splat.wav")
+var sound_reload = load("res://Entities/Weapons/Sounds/Reload.wav")
+
 var delay_between_shots : float = 0.2
 var reload_time : float = 2.0
 
@@ -24,6 +27,9 @@ func _on_shot_delay_timer_timeout():
 	print(shot_counter)
 	if shot_counter >= shots_before_reload:
 		print("Reload")
+		# Play the reload sound just before the end of the reload timer
+		var reload_sound_time : float = 0.75
+		get_tree().create_timer(reload_time - reload_sound_time).timeout.connect(_play_reload_sound)
 		# Reload weapon
 		shot_counter = 0 # Reset
 		shot_delay_timer.wait_time = reload_time
@@ -32,6 +38,11 @@ func _on_shot_delay_timer_timeout():
 		print("Fire!")
 		# Fire weapon
 		shot_counter += 1
+		sound_player.stream = sound_shoot
 		sound_player.play()
 		shot_delay_timer.wait_time = delay_between_shots
 		shot_delay_timer.start()
+
+func _play_reload_sound():
+	sound_player.stream = sound_reload
+	sound_player.play()
