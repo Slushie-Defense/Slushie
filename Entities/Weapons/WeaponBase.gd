a7c9deac15679672fb9015c858452e387e604e46
squiffel <qytiswork@gmail.com>
2023-11-04 20:50:21 -0400

    Can target lock and attack now

--- Entities/Weapons/WeaponBase.gd
+++ Entities/Weapons/WeaponBase.gd
@@ -20,21 +20,22 @@ var shot_counter : int = 1 # Start at 1
 var number_of_rays : int = 1
 var angle_range : float = 30.0  # Angle range in degrees
 
+# Enemy targeting
+@onready var shapecast_2d : ShapeCast2D = $ShapeCast2D
+@export var autotarget_enemy : bool = true
+
 # Targeting
 var attack_range : float = 0.0
-var attack_target_position : Vector2 = Vector2.ZERO
+var attack_target_position : Vector2 = Vector2(0, 512)
+var attack_damage : int = 10
 
-# Does nothing at the moment
-var attack_damage : int = 5
-
-var attack_direction : Vector2 = Vector2.ZERO
-var explode_at_target : bool = false
+# Explosions
+@export var explode_at_target : bool = false
+var explosion_scene = load("res://Entities/Explosion/ExplosionAOE.tscn")
 
 func _ready():
 	shot_delay_timer.wait_time = delay_between_shots
 	shot_delay_timer.start()
-	# Test shot
-	attack_target_position = Vector2(500, 500)
 
 func _on_shot_delay_timer_timeout():	
 	if shot_counter >= shots_before_reload:
@@ -65,11 +66,26 @@ func fire_weapon():
 	# Play sound
 	sound_player.stream = sound_shoot
 	sound_player.play()
+	# Sweep the area
+	find_target_position()
 	# Fire the shot
 	fire_ray_cast()
+	# Draw the line
+	draw_line2d(raycast_2d)
+
+func find_target_position():
+	if autotarget_enemy:
+		# Completely random prioritization. Only searches for one enemy and aims at it, whatever it is
+		# Doing this as an optimization
+		shapecast_2d.force_shapecast_update() # Only enabled during the shot fired
+		if shapecast_2d.is_colliding():
+			var first_collision_result = shapecast_2d.get_collider(0)
+			if first_collision_result != null:
+				# If it hits something it can attack
+				if first_collision_result.has_method("attack"):
+					attack_target_position = first_collision_result.global_position - global_position
 
 func fire_ray_cast():
-	print("Raycast fired")
 	raycast_2d.target_position = attack_target_position
 	raycast_2d.force_raycast_update()
 	# See what it hit
@@ -81,9 +97,11 @@ func fire_ray_cast():
 			var attack = Attack.new()
 			attack.damage = attack_damage
 			first_collision_result.attack(attack)
+
+func draw_line2d(passed_raycast):
 	# Line 2D
-	var global_position_of_hit : Vector2 = raycast_2d.get_collision_point()
-	var relative_position_of_hit : Vector2 = attack_target_position if first_collision_result == null else global_position_of_hit - global_position 
+	var global_position_of_hit : Vector2 = passed_raycast.get_collision_point()
+	var relative_position_of_hit : Vector2 = global_position_of_hit - global_position if passed_raycast.is_colliding() else attack_target_position 
 	line_2d.points = [Vector2.ZERO, relative_position_of_hit]
 	get_tree().create_timer(0.1).timeout.connect(clear_line_2d)
 
