65a3f92e74b68ae0a463eb35063f936400d10533
Carlos Michael <carlosmichaeldev@gmail.com>
2023-11-13 08:11:59 -0500

    chagned to vector2 to solve

--- Entities/Managers/WaveManager/EnemySpawnInfo.gd
+++ Entities/Managers/WaveManager/EnemySpawnInfo.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var enemies: Array[PackedScene]
 @export var type: String
-@export var portals: Array[Portal]
+@export var portals: Array[Vector2]
 @export var number_to_spawn_at_once = 1
 @export var total_time: float
 @export var number_to_spawn: int
