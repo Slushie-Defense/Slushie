4be90f61162e8cec960163f7e9d395c390eaacc0
squiffel <qytiswork@gmail.com>
2023-11-21 20:38:15 -0500

    Portal changes

--- Entities/Managers/WaveManager/EnemySpawnInfo.gd
+++ Entities/Managers/WaveManager/EnemySpawnInfo.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var enemies: Array[PackedScene]
 @export var type: String
-@export var portals: Array
+@export var portals: Array[Node2D]
 @export var number_to_spawn_at_once = 1
 @export var total_time: float
 @export var number_to_spawn: int
