2d90f8cedb08aec95d0a287e6e22ccc7d0aca3b7
squiffel <qytiswork@gmail.com>
2023-11-23 15:04:10 -0500

    death

--- UserInterface/DeathScreen/DeathScreen.gd
+++ UserInterface/DeathScreen/DeathScreen.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var timer : Timer = $Timer
+
 @onready var background_panel : Panel = $BackgroundPanel
 var background_panel_style : StyleBoxFlat = StyleBoxFlat.new()
 
@@ -9,6 +11,7 @@ var camera_center = Vector2(camera_width * 0.5, camera_height * 0.5)
 
 var portal_sprite = load("res://Sprites/DeathScreen/PortalCircle.png")
 var portal_sprite_size = Vector2(512, 512)
+var portal_list : Array = []
 
 @onready var player_node : Sprite2D = $Sprite2D
 var player_rotation_speed = 10
@@ -24,12 +27,41 @@ func _ready():
 	background_panel.size = Vector2(camera_width, camera_height)
 	# Position sprite
 	player_node.position = camera_center
+	player_node.scale = Vector2(12,12)
+	# Create circle
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	player_node.rotation_degrees -= player_rotation_speed * delta
 	player_node.position.y = camera_center.y + sin(player_node.rotation * 2) * 32
+	# Portals
+	_update_portals(delta)
+	
+func _update_portals(delta):
+	# grow and spin
+	var pop_index = -1
+	if portal_list.size() > 0:
+		for i in portal_list.size():
+			var portal_spin_speed = 15
+			portal_list[i].scale = portal_list[i].scale * 1.01
+			portal_list[i].rotation_degrees += portal_spin_speed * delta
+			if portal_list[i].scale.x > 9.0:
+				portal_list[i].queue_free()
+				pop_index = i
+				
+	if not pop_index == -1:
+		portal_list.pop_at(pop_index)
 
 func _create_portal_sprite():
-	#var portal
-	return
+	var portal_circle : Sprite2D = Sprite2D.new()
+	portal_circle.texture = portal_sprite
+	portal_circle.position = camera_center
+	portal_circle.scale = Vector2(0.02, 0.02)
+	add_child(portal_circle)
+	portal_list.push_back(portal_circle)
+
+func _on_timer_timeout():
+	_create_portal_sprite()
+	timer.wait_time = 1.0
+	timer.start()
