93e0e767d5000ce6f42285febef1198742a37a56
squiffel <qytiswork@gmail.com>
2024-06-18 11:42:43 -0400

    endpoint

--- Entities/Structures/AnimatedStructure/AnimatedStructure.gd
+++ Entities/Structures/AnimatedStructure/AnimatedStructure.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @onready var turret : Sprite2D = $Turret
 @onready var base : Sprite2D = $Base
+@onready var end_point : Sprite2D = $Turret/EndPoint
 
 var turret_instant = load("res://Sprites/Structures/Instant/Slushie_Final_Turrets_Single_Base.png")
 var base_instant = load("res://Sprites/Structures/Instant/Slushie_Final_Turrets_Single_Leg.png")
@@ -29,8 +30,6 @@ func _ready():
 func _change_weapon(weapon):
 	# Baseline
 	downward_angle_limit = 15
-	turret.offset = Vector2.ZERO
-	turret.position = Vector2.ZERO
 	turret.z_index = 0
 	base.z_index = 0
 	sprite_angle_shift = 90
@@ -44,6 +43,7 @@ func _change_weapon(weapon):
 		turret.offset = Vector2(0,0)
 		sprite_angle_shift = 0
 		downward_angle_limit = 45
+		end_point.position = Vector2(48,-4)
 	elif weapon == "Projectile":
 		turret.texture = turret_projectile
 		base.texture = base_projectile
@@ -52,33 +52,41 @@ func _change_weapon(weapon):
 		turret.offset = Vector2(16,20)
 		sprite_angle_shift = 0
 		downward_angle_limit = 45
+		end_point.position = Vector2(64,4)
 	elif weapon == "Siege":
 		turret.texture = turret_siege
 		base.texture = base_siege
 		# Properties
 		base.z_index = 1
-		turret.position = Vector2(0,24)
-		turret.offset = Vector2(0,-24)
+		turret.position = Vector2(0,-16)
+		turret.offset = Vector2(16,0)
 		downward_angle_limit = 15
-		sprite_angle_shift = 90
+		sprite_angle_shift = 0
+		end_point.position = Vector2(64,4)
 
 func _process(delta):
 	var target_position = get_global_mouse_position()
 	var turret_angle = global_position.angle_to_point(target_position)
+	
+	"""
 	var turret_deg = rad_to_deg(turret_angle)
 	var turret_clamped = turret_deg
-	
 	if turret_deg > downward_angle_limit and turret_deg < 90:
 		turret_clamped = downward_angle_limit
 	if turret_deg > 90 and turret_deg < 180 - downward_angle_limit:
 		turret_clamped = 180 - downward_angle_limit
-
-	turret.rotation = deg_to_rad(turret_clamped) + deg_to_rad(sprite_angle_shift)
+	"""
+	
+	turret.rotation = turret_angle + deg_to_rad(sprite_angle_shift)
 	
 	var shoot = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
 	if shoot:
-		turret.scale.x = 0.8
-		turret.scale.y = 1.2
+		_shoot_animation()
 
+	# Reset
 	turret.scale.x = lerpf(turret.scale.x, 1.0, 0.1)
 	turret.scale.y = lerpf(turret.scale.y, 1.0, 0.1)
+
+func _shoot_animation():
+	turret.scale.x = 0.8
+	turret.scale.y = 1.2
